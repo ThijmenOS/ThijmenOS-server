c9b1b9367970b585ad52f271870a2604f0e89ae7
Thijmen Brand <thijmen@ik.nu>
2022-12-01 15:47:45 +0100

    documentation

--- src/routes/baseRoutes.ts
+++ src/routes/baseRoutes.ts
@@ -4,13 +4,13 @@ import RootRoutes from "./rootRoutes";
 import SettingsRoutes from "./settingsRoutes";
 
 export class BaseRoutes {
-  app: express.Application;
+  private readonly app: express.Application;
 
   constructor(app: express.Application) {
     this.app = app;
   }
 
-  public defineRoutes() {
+  public defineRoutes(): void {
     this.app.use("/filesystem", new FileSystemRoutes().routes);
     this.app.use("/root", new RootRoutes().routes);
     this.app.use("/settings", new SettingsRoutes().routes);
