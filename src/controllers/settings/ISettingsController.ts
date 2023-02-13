2cbf01312671034c7a4f86e3ccfaf69fc7d4f517
Thijmen Brand <thijmen@ik.nu>
2023-02-13 13:17:55 +0100

    split user settings

--- src/controllers/settings/ISettingsController.ts
+++ src/controllers/settings/ISettingsController.ts
@@ -6,13 +6,9 @@ import {
 } from "@thijmen-os/common";
 
 interface ISettingsController {
-  SetBackground(ImagePath: Path): Promise<void>;
-  GetBackground(): Promise<string>;
   GrantPermission(props: PermissionRequestDto): Promise<boolean>;
   RevokePermission(props: PermissionRequestDto): Promise<boolean>;
   RevokeAllPermissions(applicationId: string): Promise<boolean>;
-  ReadUserSettings(userId: string): Promise<any>;
-  ReadGlobalSettings(userId: string): Promise<OSSettings>;
   GetAllUsers(): Promise<Array<User>>;
 }
 
